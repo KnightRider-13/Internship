bb06dfdd2c6562ee76f8782c86261963a2e57d0f
KnightRider-13 <fahmay17@gmail.com>
2024-08-19 20:21:29 +0200

    Dynamic Explore Page

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/styles/style.css";
-import Timer from "./Timer";
+import NftTile from "../UI/NftTile";
 
 const NewItems = () => {
   const dataURL =
@@ -75,83 +74,28 @@ const NewItems = () => {
             </div>
           </div>
           <Slider {...sliderSettings}>
-            {isLoaded
-              ? newItemData.map((nft) => (
-                  <div
-                    className="col-lg-12 col-md-12 col-sm-12 col-xs-12"
-                    key={nft.id}
-                  >
-                    <div className="nft__item">
-                      <div className="author_list_pp">
-                        <Link
-                          to={`/author/${nft.authorId}`}
-                          data-bs-toggle="tooltip"
-                          data-bs-placement="top"
-                          title={nft.title}
-                        >
-                          <img className="lazy" src={nft.authorImage} alt="" />
-                          <i className="fa fa-check"></i>
-                        </Link>
-                      </div>
-                      {nft.expiryDate ? (
-                        <div className="de_countdown"><Timer expiryDate={nft.expiryDate}/></div>
-                      ) : null}
-                      <div className="nft__item_wrap">
-                        <div className="nft__item_extra">
-                          <div className="nft__item_buttons">
-                            <button>Buy Now</button>
-                            <div className="nft__item_share">
-                              <h4>Share</h4>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-facebook fa-lg"></i>
-                              </a>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-twitter fa-lg"></i>
-                              </a>
-                              <a href="">
-                                <i className="fa fa-envelope fa-lg"></i>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-
-                        <Link to={`/item-details/${nft.nftId}`}>
-                          <img
-                            src={nft.nftImage}
-                            className="lazy nft__item_preview"
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="nft__item_info">
-                        <Link to={`/item-details/${nft.nftId}`}>
-                          <h4>{nft.title}</h4>
-                        </Link>
-                        <div className="nft__item_price">{nft.price}</div>
-                        <div className="nft__item_like">
-                          <i className="fa fa-heart"></i>
-                          <span>{nft.likes}</span>
-                        </div>
-                      </div>
+            {isLoaded ? (
+              newItemData.map((nft) => 
+                <NftTile nfts={nft}key={nft.id} />
+              )
+            ) : (
+              new Array(4).fill(0).map((_, index) => (
+                <div
+                  className="col-lg-12 col-md-12 col-sm-12 col-xs-12"
+                  key={index}
+                >
+                  <div className="nft__item">
+                    <div className="author_list_pp skeleton authorImage__loading"></div>
+                    <div className="nft__item_wrap skeleton"></div>
+                    <div className="nft__item_info">
+                      <div className="header--loading skeleton"></div>
+                      <div className="nft__item_price item__price--loading skeleton"></div>
+                      <div className="nft__item_like item__like--loading skeleton"></div>
                     </div>
                   </div>
-                ))
-              : new Array(4).fill(0).map((_, index) => (
-                  <div
-                    className="col-lg-12 col-md-12 col-sm-12 col-xs-12"
-                    key={index}
-                  >
-                    <div className="nft__item">
-                      <div className="author_list_pp skeleton authorImage__loading"></div>
-                      <div className="nft__item_wrap skeleton"></div>
-                      <div className="nft__item_info">
-                        <div className="header--loading skeleton"></div>
-                        <div className="nft__item_price item__price--loading skeleton"></div>
-                        <div className="nft__item_like item__like--loading skeleton"></div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                </div>
+              ))
+            )}
           </Slider>
         </div>
       </div>
